acfafc34dbec41d4df8aa1bb48b6f8b5487e58bc
ben <2390188@mail.ru>
2025-12-25 17:32:54 +0300

    добавил получение одной строки

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -12,7 +12,12 @@ class ItemCreateAPIView(APIView):
     """
 
     def post(self, request):
+        print('-----')
+        print('---')
+        print(request.data)
         serializer = ItemCreateSerializer(data=request.data)
+        print('====')
+        print(serializer)
         if serializer.is_valid():
             item = serializer.save()
             return Response(
@@ -52,6 +57,13 @@ class ItemDetailAPIView(APIView):
             return Item.objects.get(id=item_id)
         except Item.DoesNotExist:
             return None
+
+    def get(self, request, item_id):
+        item = self.get_object(item_id)
+        if not item:
+            return Response({"detail": "Item not found"}, status=404)
+        serializer = ItemCreateSerializer(item)
+        return Response(serializer.data)
 
     def patch(self, request, item_id):
         item = self.get_object(item_id)
